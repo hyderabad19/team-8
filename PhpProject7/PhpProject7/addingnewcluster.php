10204de78e27a0a5b640f2555224e801b33adff2
Ramya <ramyasree0299@gmail.com>
2019-07-14 10:56:44 +0530

    Final_push

--- PhpProject7/PhpProject7/addingnewcluster.php
+++
@@ -1,7 +0,0 @@
-
-
- <?php
-$con=mysqli_connect("localhost","root","","loop");
-$value=$_POST['addnewcluster'];
-$query = mysqli_query($con,"insert into cluster(clustername) values('$value')");
-?>
--- PhpProject7/PhpProject7/validateadmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-        // put your code here
-        $email=$_POST['email'];
-        $password=$_POST['password'];
-        if($email == "" && $password="")
-        {
-           header("location:clusteradd.php");
-        }
-?>
